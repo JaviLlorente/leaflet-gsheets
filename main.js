f1b3ba10f6981fb6d4de6328de1f21d650ef108e
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2024-02-07 19:31:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -210,6 +210,7 @@ function addPoints(data) {
 					'Pk: ' + e.target.feature.properties.Pk + '<br/>' +
 					//'Foto: ' + e.target.feature.properties.Foto + '<br/>' +	
 					'<a href="' + e.target.feature.properties.Foto + '">Descarga la foto del atropello</a><br/>'  
+					'<img src="' + e.target.feature.properties.Foto + '" width="250"><br/>'  //Esto funciona con las de Jotform
 					//'Observaciones: ' + e.target.feature.properties.Observaciones + '<br/>' +
 					//Funcionan estos formatos de foto + el id al final: https://drive.google.com/uc?id= // https://drive.google.com/uc?export=download&id=
 					//No funciona'<iframe src="' + e.target.feature.properties.Foto + '" name="iframe_a" width="250"></iframe>' + '<br/>' +						
