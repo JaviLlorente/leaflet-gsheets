f13ac073337059fc420479070797f47263b9cc57
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2024-09-15 13:43:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -242,7 +242,8 @@ function addPoints(data) {
 					'Carretera: ' + e.target.feature.properties.Carretera + '<br/>' +	
 					'Pk: ' + e.target.feature.properties.Pk + '<br/>' +
 					//'Foto: ' + e.target.feature.properties.Foto + '<br/>' +	
-					'<a href="' + e.target.feature.properties.Foto + '" target="_blank" rel="noopener noreferrer" >Descarga la foto del atropello</a><br/>' +
+					'El enlace a fotos desde el mapa ha dejado de funcionar. Si quieres descargar la foto, busca el atropello en la Base de datos'
+					//'<a href="' + e.target.feature.properties.Foto + '" target="_blank" rel="noopener noreferrer" >Descarga la foto del atropello</a><br/>' +
 					//'<img src="' + e.target.feature.properties.Foto + '" width="250"><br/>'  //Esto funciona con las de Jotform
 					//'Observaciones: ' + e.target.feature.properties.Observaciones + '<br/>' +
 					//Funcionan estos formatos de foto + el id al final: https://drive.google.com/uc?id= // https://drive.google.com/uc?export=download&id=
